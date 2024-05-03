00c3786dcca400805445efb33aa228521dad62d7
anburocky3 <15264938+anburocky3@users.noreply.github.com>
2024-05-03 14:00:47 +0530

    fix: appName typo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg  bg-primary" data-bs-theme="dark">
             <div className="container">
-                <a className="navbar-brand" href="#">MovieArea</a>
+                <a className="navbar-brand" href="#">MovieAreas</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
